5e4ba58e55a9a927bbc4de74c85f30d7c4aa7856
Your Name <you@example.com>
2023-07-02 16:58:28 -0400

    Update: <brief description of changes>

--- src/components/pages/Support.js
+++ src/components/pages/Support.js
@@ -6,6 +6,61 @@ import "./Support.css";
 export function Support(){
     return(
         <Container>
+          <Row>
+                <Col>
+                <h2 className='Contact'>Contact</h2>
+                <p className='Contact-body'>Settled your decision? Great! You can reach us by emailing us at uOttawaVolleybal@gmai.com or call us at 1-999 555 7766. <br/>
+                We are located at 5677 Minto Ave, Ottawa, ON, Canada. 
+                We are open everyday from 10:00am to 6:00pm
+                </p>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                <h2 className='Contact'>Our Team</h2>
+                <p className='Contact-body'>Our esteemed team of volleyball coaches is dedicated to providing 
+                exceptional assistance to each and every student. With their vast experience and deep understanding 
+                of the game, they offer personalized guidance tailored to individual needs. They excel at honing technical 
+                skills, refining strategies, and instilling a strong work ethic. Their passion for teaching and commitment 
+                to the success of their students make them highly effective mentors. Join our coaching 
+                program and experience firsthand the transformative impact of their expertise and support.
+                </p>
+                </Col>
+            </Row>
+
+            
+            <Row>
+                <Col>
+                <h2 className='Contact'>Participate in Leagues!</h2>
+                <p className='Contact-body'>Unlock international league opportunities and compete on a global stage 
+                with our local university volleyball services providing 
+                network with talent scouts and international league representatives as we participate in local and 
+                international events 
+                to secure your chance in international leagues by join our program, bridge the
+                 gap, and be noticed by industry experts for international volleyball events!
+                </p>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                <h2 className='Contact'>Tips and Tricks</h2>
+                <p className='Contact-body'>Need help positioning, try to Communicate and anticipate plays with 
+                teammates and maintain a balanced stance and be aware of court boundaries.
+                Cant rebound correctly? Watch the ball, move quickly, and time your jump. Box out opponents and 
+                develop strong wrists and hands.
+                Want to smach like pro players? Begin your approach by taking quick and explosive steps towards
+                 the ball, maintain a steady rhythm and build up momentum as you approach the hitting zone. Time your
+                jump appropriately so that you reach the peak of your jump just as the ball is in the perfect 
+                hitting position. This allows you to generate maximum power and accuracy in your smash, and work on 
+                your arm Swing: As you jump, draw your dominant arm back, preparing for a strong and explosive swing.
+                 Swing your arm forward forcefully, extending it fully at the point of contact with the ball. 
+                 Aim to contact the ball at the highest point possible.
+                 There you go!
+                </p>
+                </Col>
+            </Row>
          
         </Container>
         
